807f93288f830b2ec80effe0e62a24125a4f6fc2
luchkob <umy451710@gmail.com>
2023-07-25 07:54:47 +0700

    ok

--- src/app/Components/Home.js
+++ src/app/Components/Home.js
@@ -1,7 +1,9 @@
-
+ "use client"
 import React from 'react'
 import { auth } from '../db/firebase_config'
 import { signOut } from 'firebase/auth'
+ import { CldVideoPlayer } from 'next-cloudinary'
+ 
 function Home() {
  async function logOut(){
    signOut(auth)
@@ -17,16 +19,22 @@ function Home() {
     <div className='fixed items-center bg-blue-600 p-5 w-screen flex justify-between  '>
        <div className='flex '>
         <button onClick={()=>logOut()}className="bg-white  rounded px-4 py">L0GOUT</button>
-         <img className='inline ml-3 bg-transparent text-blue-500' width={20} height={20}  src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDed61nMZ0kLsFJGTkmPJDSPsKp9xlr-dwvTnOrprMuQ&s'></img>
+        <img className='rounded-lg ml-2' src={auth.currentUser.photoURL} width={40} height={40} alt='sorry'/>
+         {/* <img className='inline ml-3 bg-transparent text-blue-500' width={20} height={20}  src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDed61nMZ0kLsFJGTkmPJDSPsKp9xlr-dwvTnOrprMuQ&s'></img> */}
        </div>
          <span>
          <svg xmlns="http://www.w3.org/2000/svg" fill="none"viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
              <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
              </svg>
+             
          </span>
+         <div className='text-white text-lg absolute right-16 top-6 '>{auth.currentUser.displayName }</div>
     </div>
-      <div className='translate-y-20'>
-        <h1>YOU are :{auth.currentUser.email}</h1>
+      <div className='translate-y-20 mx-8  '>
+           <h1 className='text-2xl '>Welcom puk ah cherm</h1>
+           <p>This is a sample project created using NextJS, Firebase and TailwindCSS.</p><br/>
+           
+ 
       </div>
     </>
 
--- src/app/Components/Login.js
+++ src/app/Components/Login.js
@@ -2,13 +2,40 @@
 import React, { useRef } from 'react'
 import { useState,useContext } from 'react'
 import { AuthContext } from '../page'
-import { createUserWithEmailAndPassword,signInWithEmailAndPassword } from 'firebase/auth'
+import { createUserWithEmailAndPassword,signInWithEmailAndPassword ,GoogleAuthProvider,signInWithRedirect,getRedirectResult} from 'firebase/auth'
 import { auth } from '../db/firebase_config'
+import {FcGoogle} from 'react-icons/fc'
+const provider =new GoogleAuthProvider()
+
 export default function Login() {
      const [input,setInput]=useState({email:'',password:""})
       const {login,setlogin}=useContext(AuthContext)
       const [error,SetError]=useState(false)
 
+      function GoogleSignin(){
+        signInWithRedirect(auth, provider);
+        getRedirectResult(auth)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access Google APIs.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+     setlogin(!login)
+    // The signed-in user info.
+    const user = result.user;
+    // IdP data available using getAdditionalUserInfo(result)
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+    alert('please try again')
+  });
+      }
      function loginUser(){
          const email_req=/^[a-zA-Z0-9_\.\+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-\.]+/
          const password_req= /^[A-Za-z]\w{7,14}$/
@@ -61,9 +88,12 @@ export default function Login() {
         )}
     <input   value={input.email} onChange={(e)=>{setInput({...input,email:e.target.value})}} type='email' placeholder='email!' className='border-b-2 border-b-blue-600 outline-none mb-2'/>
     <input  value={input.password} onChange={(e)=> setInput({...input,password:e.target.value})} type='text' placeholder='password' className=' border-red-500 outline-none   border-b-2 mb-2'/>
-    <div className='flex gap-3 uppercase text '>
+    <div className='flex gap-3 uppercase text items-center justify-center'>
     <input onClick={loginUser} type='submit' value='login' className='hover:text-red-500'/>
     <input onClick={singup} type='submit' value='singup' className='hover:text-blue-700'/>
+    <div className='block mt-2' onClick={GoogleSignin}>
+       <FcGoogle/>
+    </div>
     </div>
     </form>
     </div>
